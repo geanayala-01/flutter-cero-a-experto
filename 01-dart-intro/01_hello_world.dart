451ae056fc7827f683c1617e7e89efc30e35f23b
Gean Pierre Ayala <gean.ayala.dev@gmail.com>
2026-09-19 19:10:53 -0500

    01-dart-intro: hello world y variables

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  // var myName = 'Fernando';
+  // let final myName;
+  // String myName = 'Fernando';
+  const myName = 'Fernando';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+  
+}
